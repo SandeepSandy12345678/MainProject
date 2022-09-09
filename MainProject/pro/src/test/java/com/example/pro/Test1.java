bd9ec138bbd77c61025b673413107811a973afb5
SandeepSandy12345678 <98653089+SandeepSandy12345678@users.noreply.github.com>
2022-09-09 12:01:28 +0530

    Add files via upload

---
+++ MainProject/pro/src/test/java/com/example/pro/Test1.java
@@ -0,0 +1,25 @@
+package com.example.pro;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
+@SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
+public class Test1 {
+@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	public void adding_New_Farmer_Succesfully() throws Exception {
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/farmer/addnewfarmer_v3?fname=sandepp_rr&age=24&gender=Male",
+				String.class)).contains("New farmer added successfully");
+	}
+}
